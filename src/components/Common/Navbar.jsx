9e6223d254a7f8d1559bb4057eea67f868f726d4
Nandani Kumari <nandani@contaque.com>
2024-05-11 11:32:02 +0530

    Updates

--- src/components/Common/Navbar.jsx
+++ src/components/Common/Navbar.jsx
@@ -11,24 +11,24 @@ import { categories } from "../../services/apis"
 import { ACCOUNT_TYPE } from "../../utils/constants"
 import ProfileDropdown from "../core/Auth/ProfileDropdown"
 
-// const subLinks = [
-//   {
-//     title: "Python",
-//     link: "/catalog/python",
-//   },
-//   {
-//     title: "javascript",
-//     link: "/catalog/javascript",
-//   },
-//   {
-//     title: "web-development",
-//     link: "/catalog/web-development",
-//   },
-//   {
-//     title: "Android Development",
-//     link: "/catalog/Android Development",
-//   },
-// ];
+const subLinks = [
+  {
+    title: "Python",
+    link: "/catalog/python",
+  },
+  {
+    title: "javascript",
+    link: "/catalog/javascript",
+  },
+  {
+    title: "web-development",
+    link: "/catalog/web-development",
+  },
+  {
+    title: "Android Development",
+    link: "/catalog/Android Development",
+  },
+];
 
 function Navbar() {
   const { token } = useSelector((state) => state.auth)
@@ -52,7 +52,7 @@ function Navbar() {
     })()
   }, [])
 
-  // console.log("sub links", subLinks)
+  console.log("sub links", subLinks)
 
   const matchRoute = (route) => {
     return matchPath({ path: route }, location.pathname)
